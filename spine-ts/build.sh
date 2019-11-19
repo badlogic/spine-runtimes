41be52394efe59fcfc7454cf5d5cf5724853ee7a
badlogic <badlogicgames@gmail.com>
2019-11-19 14:26:03 +0100

    TS.

--- spine-ts/build.sh
+++ spine-ts/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-set -e -x
+set -e
+
+BRANCH="$(git symbolic-ref --short HEAD)"
+echo "Building spine-ts $BRANCH artifacts"
 tsc -p tsconfig.json
 tsc -p tsconfig.core.json
 tsc -p tsconfig.webgl.json
@@ -7,3 +10,12 @@ tsc -p tsconfig.canvas.json
 tsc -p tsconfig.threejs.json
 tsc -p tsconfig.player.json
 ls build/*.js build/*.ts | awk '{print "unexpand -t 4 ", $0, " > /tmp/e; mv /tmp/e ", $0}' | sh
+
+if ! [ -z "$TS_UPDATE_URL" ];
+then
+    echo "Deploying spine-ts $BRANCH artifacts"
+    zip -j spine-ts.zip build/* player/css/spine-player.css player/example/external/*
+    curl -F "file=@spine-ts.zip" "$TS_UPDATE_URL$BRANCH"
+else
+    echo "Not deploying artifacts. TS_UPDATE_URL and BRANCH are not set."
+fi
